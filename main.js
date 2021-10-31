db1339157360f02c86a38fd91f803462eace465a
Randy Ruiz <randyruiz@Randys-MBP.fios-router.home>
2021-10-31 19:55:40 -0400

    finished

---
+++ main.js
@@ -0,0 +1,22 @@
+const paragraph = document.querySelector('p');
+paragraph.style.color = 'lightblue';
+
+const h1 = document.querySelector('h1');
+h1.style.fontSize = '48px';
+
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.';
+
+const item13 = document.querySelector('#item-13');
+item13.style.opacity = 0.5;
+
+const item3 = document.getElementById('item-3').innerText = 'I say, "Hi"';
+
+const image = document.querySelector('img');
+image.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg'
+image.style.height = '300px';
+
+const dog = document.querySelector('.dog');
+dog.style.height = '300px';
+
+const item16 = document.querySelector('#item-16');
+item16.className = 'item';
